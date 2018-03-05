939860e24e999a37bf8ec11293a7e7d3ba3e24b8
 WibeTeam <wibeteam@WibeTeams-Mac-mini.local>
2018-03-05 17:58:08 +0200

    resort (part 1)

--- controllers/index.js
+++ controllers/index.js
@@ -20,45 +20,63 @@ function getDateOfDay(type) {
         if (now.getMonth()<8) return 1;
         if (now.getMonth()>=8) return 0;
     }
-};
-function getUnitsInline(tabs, id) {
-    for ( let i in tabs ){
-        if (tabs[i].tabId == id){
-            return tabs[i].units_in_line;
-        }
+}
+function getStrDate(type) {
+    let now = new Date();
+    if (type === 0) return now.getFullYear() +'-0'+ now.getMonth() +'-0'+ getDateOfDay(type);
+    if (type === 1) {
+        let v = getDateOfDay(type)
+        v = new Date(v);
+        if (v < 1)
+            return now.getFullYear() +'-0'+ now.getMonth()-1 +'-0'+ v.getDate();
+        else
+            return now.getFullYear() +'-0'+ now.getMonth() +'-0'+ v.getDate();
     }
-};
+    if (type === 3){
+        if ( getDateOfDay(type) === 0 )
+            return now.getFullYear() +'-08-01';
+        if ( getDateOfDay(type) === 1 )
+            return now.getFullYear()-1 +'-08-01';
+    }
+}
+function getUnitsInline(tabs, id) {
+    for ( let i in tabs )
+        if (tabs[i].tabId === id)
+            return tabs[i].units_in_line;
+}
+
 function getUnitsInlineList(tabs, idList) {
     let arr =[];
     for( let j in idList)
         for ( let i in tabs ){
-            if (tabs[i].tabId == idList[j]){
+            if (tabs[i].tabId === idList[j]){
                 arr.push( tabs[i].units_in_line);
             }
         }
     return arr;
-};
-function getTopMenuView(period, competitionId_linkpart, type) {
+}
+
+function getTopMenuView(params, type) {
     let link1, link2, link3;
-    if (period==='0') link1 = 'active';
-    if (period==='1') link2 = 'active';
-    if (period==='3') link3 = 'active';
+    if (params.period==='0') link1 = 'active';
+    if (params.period==='1') link2 = 'active';
+    if (params.period==='3') link3 = 'active';
     return `
             <nav>
-                <div class="item ${ link1 } first"><a href="${'/'+type+'/0/'+ competitionId_linkpart}">Today</a></div>
-                <div class="item ${ link2 }"><a href="${  '/'+type+'/1/'+ competitionId_linkpart}">The week</a></div>
-                <div class="item ${ link3 } last"><a href="${  '/'+type+'/3/'+ competitionId_linkpart}">Season</a></div>
+                <div class="item ${ link1 } first"><a href="/${type+'/0/'+ params.competitionId+'/'+params.resort}/">Today</a></div>
+                <div class="item ${ link2 }"><a href="${ '/'+type+'/1/'+ params.competitionId+'/'+params.resort}/">The week</a></div>
+                <div class="item ${ link3 } last"><a href="${ '/'+type+'/3/'+ params.competitionId+'/'+params.resort}/">Season</a></div>
             </nav>
         `
 }
-function getAwardsListView(tabs, period, competitionId) {
+function getAwardsListView(tabs, params) {
     let str='';
     for ( let i in tabs ){
         let active='';
-        if (competitionId === tabs[i].tabId) active='active';
+        if (params.competitionId === tabs[i].tabId) active='active';
         str += `
             <div class="item ${active}" id="${tabs[i].tabId}">
-                <a href="/list/${period}/${tabs[i].tabId}/" id="${tabs[i].tabId}" >
+                <a href="/list/${params.period}/${tabs[i].tabId}/${params.resort}/" id="${tabs[i].tabId}" >
                     <img src="/img/${tabs[i].tabIcon}" alt="${tabs[i].tabname}">
                     <div class="description">
                         <div class="tit">${tabs[i].tabname}</div>
@@ -93,6 +111,7 @@ function getFSPlayersListView(tabs, idList, json, units_list) {
     }
     return str;
 }
+
 function getPlayersListView(json, units) {
     let obj;
     try { obj = JSON.parse(json) } catch(e){console.log(e);}
@@ -141,10 +160,17 @@ function getPlayerRowView(obj, units, id) {
         </div>`
 }
 
+function getValid_competitionIdList(str){
+    let arr = str.split('_');
+    for (let i in arr)
+        arr[i]=parseInt(arr[i]);
+    return JSON.stringify(arr)
+}
 
 async function getResort(result, express_req, express_res, type) {
     soap.createClient(config.url, config.wsdlOptions,  function(err, client) {
         client.getSkiAreasJSON(resort_args,  function(err, result2) {
+            const tabs = config.tabs;
             let data='';
             let arr = result2.return
             try { arr = JSON.parse(arr) }catch (e){console.log(e)};
@@ -154,29 +180,26 @@ async function getResort(result, express_req, express_res, type) {
                         data+=arr[i].name+': '+arr[i].subAreas+' <br />';
 
             if (type === 'list'){
-                let period = express_req.params.period;
-                let competitionId = express_req.params.competitionId;
-                let tabs = config.tabs;
                 express_res.render('../view/'+type+'.hbs', {
                     title: config.title,
                     popup_categories_count: (config.popup_categories_count) ? config.popup_categories_count : 0 ,
-                    period: period,
-                    TopMenuView: getTopMenuView(period, competitionId, type),
-                    PlayersListView: getPlayersListView( result.return, getUnitsInline(tabs, competitionId) ),
-                    AwardsListView: getAwardsListView( tabs, period, competitionId),
+                    period: express_req.params.period,
+                    resort: express_req.params.resort,
+                    TopMenuView: getTopMenuView(express_req.params, type),
+                    PlayersListView: getPlayersListView( result.return, getUnitsInline(tabs, express_req.params.competitionId) ),
+                    AwardsListView: getAwardsListView( tabs, express_req.params),
                     CategoriesListView: getCategoriesListView(tabs),
                     data: data
                 });
             }
 
             if (type==='fs_list'){
-                let period = express_req.params.period;
+                express_req.params.competitionId = express_req.params.competitionIdList;
                 let competitionIdList = express_req.params.competitionIdList.split('_');
-                let tabs = config.tabs;
                 express_res.render('../view/'+type+'.hbs', {
                     title: config.title,
-                    period: period,
-                    TopMenuView: getTopMenuView(period, express_req.params.competitionIdList, type),
+                    period: express_req.params.period,
+                    TopMenuView: getTopMenuView(express_req.params, type),
                     // PlayersListView: result.return,
                     FSPlayersListView: getFSPlayersListView(tabs, competitionIdList, result.return, getUnitsInlineList(tabs, competitionIdList) ),
                     data: data
@@ -190,7 +213,18 @@ async function getResort(result, express_req, express_res, type) {
 
 
 module.exports = {
-    soap_request(args, express_req, express_res, type) {
+    soap_request(express_req, express_res, type) {
+        const part_of_date = getStrDate(  parseInt(express_req.params.period) )
+        console.log(part_of_date);
+        let args = {
+            credentials:config.credentials,
+            competitionId: express_req.params.competitionId,
+            period: express_req.params.period,
+            startFrom: part_of_date+'T15:20:54.120+04:00',
+        };
+        const resort = express_req.params.resort;
+        // if (resort !== 'overall') args.skiAreaId = resort;
+
         soap.createClient(config.url, config.wsdlOptions,  function(err, client) {
             client.getCompetitionDataJSON(args, function(err, result) {
                 if (err) console.log(err);
@@ -198,37 +232,22 @@ module.exports = {
             });
         });
     },
-    soap_request_for_many(args, express_req, express_res, type){
-        console.log(args.competitionIdsJSON);
+
+    soap_request_for_many(express_req, express_res, type){
+        const partofdate = getStrDate(  parseInt(express_req.params.period) )
+        const valid_competitionIdList = getValid_competitionIdList(express_req.params.competitionIdList);
+        const args = {
+            credentials:config.credentials,
+            competitionIdsJSON: valid_competitionIdList,
+            period: express_req.params.period,
+            startFrom: partofdate+'T15:20:54.120+04:00'
+        };
         soap.createClient(config.url, config.wsdlOptions,  function(err, client) {
             client.getCompetitionDataJSON2(args, function(err, result) {
                 if (err) console.log(err);
                 getResort(result, express_req, express_res, type);
             });
         });
-    },
-    getStrDate(type) {
-        let now = new Date();
-        if (type === 0) return now.getFullYear() +'-0'+ now.getMonth() +'-'+ getDateOfDay(type);
-        if (type === 1) {
-            let v = getDateOfDay(type)
-            v = new Date(v);
-            if (v < 1)
-                return now.getFullYear() +'-0'+ now.getMonth()-1 +'-'+ v.getDate();
-            else
-                return now.getFullYear() +'-0'+ now.getMonth() +'-'+ v.getDate();
-        }
-        if (type === 3){
-            if ( getDateOfDay(type) === 0 )
-                return now.getFullYear() +'-08-01';
-            if ( getDateOfDay(type) === 1 )
-                return now.getFullYear()-1 +'-08-01';
-        }
-    },
-    getValid_competitionIdList(str){
-        let arr = str.split('_');
-        for (let i in arr)
-            arr[i]=parseInt(arr[i]);
-        return JSON.stringify(arr)
     }
+
 }
